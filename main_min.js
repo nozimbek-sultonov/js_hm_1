56a70391968c18d7d72713d849cb341247ac2ce3
nozimbek-sultonov <75173590+nozimbek-sultonov@users.noreply.github.com>
2021-02-22 12:18:01 +0200

    Add files via upload

---
+++ main_min.js
@@ -0,0 +1,9 @@
+confirm("Brat / Sestrichka: Dollar ni kursini ko'rasizmi :) ");
+var first = 10532.3
+var sum = prompt("So'm yozing -> (Dollar) ")-0
+console.log(sum / first);
+confirm("Rubl kursini ko'rasizmi");
+var first = 142.360
+var sum = prompt("So'm yozing -> (Rubl) ") - 0
+alert("Tugadi :)(: Tugadi")
+console.log(sum / first);
